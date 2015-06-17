3b45712ea6c65a3fe70eb958783acd2b1d0f37df
Franklin Wise <franklin@krave.io>
2015-06-17 13:00:20 -0700

    s3

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"github.com/rlmcpherson/s3gof3r"
 	"os"
 	"strings"
 )
@@ -35,7 +36,39 @@ func main() {
 	for _, lines := range rawCSVdata {
 		for _, line := range lines {
 			uid := strings.Split(line, ":")[1]
-			fmt.Println(uid)
+			if hit, _ := get(uid); hit {
+				fmt.Printf("%s hit\n", uid)
+			} else {
+				fmt.Printf("%s miss\n", uid)
+			}
 		}
 	}
+}
+
+const s3Root = "s3-us-west-2.amazonaws.com"
+const bucketName = "images.takuapp.com"
+
+func get(path string) (bool, error) {
+	//we get keys everytime because they can expire... this could be improved
+	keys, err := s3gof3r.InstanceKeys() // get S3 keys
+	if err != nil {
+		return false, err
+	}
+
+	s3 := s3gof3r.New(s3Root, keys)
+
+	b := s3.Bucket(bucketName)
+
+	rc, _, err := b.GetReader(path, nil)
+	if err != nil {
+		return false, err
+	}
+	defer rc.Close()
+	buffer := make([]byte, 512)
+	_, err = rc.Read(buffer)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
