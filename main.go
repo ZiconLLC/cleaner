4a7ad38fcf03e7da868c504994aebf725b51ab33
Franklin Wise <franklin@krave.io>
2015-06-17 16:01:00 -0700

    update

--- main.go
+++ main.go
@@ -5,9 +5,10 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"github.com/rlmcpherson/s3gof3r"
 	"os"
 	"strings"
+
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 func main() {
@@ -34,6 +35,8 @@ func main() {
 		os.Exit(1)
 	}
 
+	exists := getKeys()
+
 	hits := make([]string, 0)
 	misses := make([]string, 0)
 
@@ -49,7 +52,8 @@ func main() {
 	for _, lines := range rawCSVdata {
 		for i, line := range lines {
 			uid := strings.Split(line, ":")[1]
-			if hit, _ := get(uid); hit {
+
+			if _, hit := exists[uid]; hit {
 				hits = append(hits, uid)
 				//	fmt.Printf("%s hit\n", uid)
 			} else {
@@ -80,27 +84,51 @@ func main() {
 const s3Root = "s3-us-west-2.amazonaws.com"
 const bucketName = "images.takuapp.com"
 
-func get(path string) (bool, error) {
-	//we get keys everytime because they can expire... this could be improved
-	keys, err := s3gof3r.InstanceKeys() // get S3 keys
-	if err != nil {
-		return false, err
+//func get(path string) (bool, error) {
+//	//we get keys everytime because they can expire... this could be improved
+//	keys, err := s3gof3r.InstanceKeys() // get S3 keys
+//	if err != nil {
+//		return false, err
+//	}
+//
+//	s3 := s3gof3r.New(s3Root, keys)
+//
+//	b := s3.Bucket(bucketName)
+//
+//	rc, _, err := b.GetReader(path, nil)
+//	if err != nil {
+//		return false, err
+//	}
+//	rc.Close()
+//	//buffer := make([]byte, 100)
+//	//_, err = rc.Read(buffer)
+//	//if err != nil {
+//	//	return false, err
+//	//}
+//
+//	return true, nil
+//}
+
+func getKeys() map[string]bool {
+	d := downloader{bucket: bucketName, keys: make(map[string]bool)}
+
+	bn := bucketName
+	prefix := ""
+	client := s3.New(nil)
+	params := &s3.ListObjectsInput{Bucket: &bn, Prefix: &prefix}
+	client.ListObjectsPages(params, d.eachPage)
+	return d.keys
+}
+
+type downloader struct {
+	bucket, dir string
+	keys        map[string]bool
+}
+
+func (d *downloader) eachPage(page *s3.ListObjectsOutput, more bool) bool {
+	for _, obj := range page.Contents {
+		d.keys[*obj.Key] = true
 	}
 
-	s3 := s3gof3r.New(s3Root, keys)
-
-	b := s3.Bucket(bucketName)
-
-	rc, _, err := b.GetReader(path, nil)
-	if err != nil {
-		return false, err
-	}
-	rc.Close()
-	//buffer := make([]byte, 100)
-	//_, err = rc.Read(buffer)
-	//if err != nil {
-	//	return false, err
-	//}
-
-	return true, nil
+	return true
 }
