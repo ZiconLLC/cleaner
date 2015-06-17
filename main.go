aad0554275a5e2bd01ee01f014784b2ddf3803b1
Franklin Wise <franklin@krave.io>
2015-06-17 16:30:41 -0700

    update

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"os"
+	"regexp"
 	"strings"
 	"sync"
-
-	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 func main() {
@@ -85,6 +86,40 @@ func main() {
 const s3Root = "s3-us-west-2.amazonaws.com"
 const bucketName = "images.takuapp.com"
 
+func getKeys(path string) map[string]bool {
+	keys := make(map[string]bool)
+	lines, err := readLines(path)
+	if err != nil {
+		fmt.Println(err.Error())
+		return keys
+	}
+	var uidRegex = regexp.MustCompile(`s3:\/\/images.takuapp.com\/(.*$)`)
+	for _, line := range lines {
+		parts := uidRegex.FindStringSubmatch(line)
+		if len(parts) > 1 {
+			keys[parts[1]] = true
+		}
+	}
+	return keys
+}
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
 //func get(path string) (bool, error) {
 //	//we get keys everytime because they can expire... this could be improved
 //	keys, err := s3gof3r.InstanceKeys() // get S3 keys
@@ -111,7 +146,7 @@ const bucketName = "images.takuapp.com"
 //}
 var wg sync.WaitGroup
 
-func getKeys() map[string]bool {
+func getKeyss3() map[string]bool {
 	d := downloader{bucket: bucketName, keys: make(map[string]bool)}
 
 	bn := bucketName
@@ -135,7 +170,7 @@ func (d *downloader) eachPage(page *s3.ListObjectsOutput, lastPage bool) bool {
 		d.keys[*obj.Key] = true
 	}
 	fmt.Println("page")
-	if !lastPage {
+	if lastPage {
 		wg.Done()
 	}
 	return true
