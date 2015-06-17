7d4539094f970591d711492820fd9024fda8f217
Franklin Wise <franklin@krave.io>
2015-06-17 13:11:57 -0700

    update

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/rlmcpherson/s3gof3r"
@@ -10,7 +11,8 @@ import (
 )
 
 func main() {
-	file := flag.String("import", "redis.csv", "a csv file")
+	file := flag.String("import", "~/redis.csv", "a csv file")
+	expFile := flag.String("export", "~/misses.json", "path to export file")
 	flag.Parse()
 	csvfile, err := os.Open(*file)
 
@@ -32,17 +34,48 @@ func main() {
 		os.Exit(1)
 	}
 
+	hits := make([]string, 0)
+	misses := make([]string, 0)
+
+	stats := func() {
+		h := len(hits)
+		m := len(misses)
+		t := h + m
+		r := h / (t * 1.0)
+		fmt.Printf("hits: %d  misses: %d  total: %d  hit-ratio: %d", h, m, t, r)
+	}
+
 	// sanity check, display to standard output
 	for _, lines := range rawCSVdata {
-		for _, line := range lines {
+		for i, line := range lines {
 			uid := strings.Split(line, ":")[1]
+			fmt.Printf("%d\n", i)
 			if hit, _ := get(uid); hit {
-				fmt.Printf("%s hit\n", uid)
+				hits = append(hits, uid)
+				//	fmt.Printf("%s hit\n", uid)
 			} else {
-				fmt.Printf("%s miss\n", uid)
+				misses = append(misses, uid)
+				//	fmt.Printf("%s miss\n", uid)
+			}
+			if i%100 == 0 {
+				stats()
 			}
 		}
 	}
+
+	fmt.Println("Finished -------------------------------------------")
+	stats()
+
+	jsondata, err := json.Marshal(misses)
+	jsonFile, err := os.Create(*expFile)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+
+	jsonFile.Write(jsondata)
+	jsonFile.Close()
 }
 
 const s3Root = "s3-us-west-2.amazonaws.com"
